133b58c9f80db1a96f9dd8d1e715fbca42ce377d
Tejas Bishnoi <tjbishnoi29@gmail.com>
2025-09-26 20:45:08 +0530

    imporoved somethings and corrected some mistakes

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,26 @@
 const express = require("express");
-
 const app = express();
-
 const port = 6969;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Sure :)');
+    console.log("Got a request on `/`. responded accordingly.");
 });
 
-app.post('/getmap', (req,res)=>{
+app.post('/getmap', (req, res) => {
     const currentusrloc = req.body;
-    console.log(`User Data Received : ${currentusrloc}`);
+
+    console.log('User Data Received:', currentusrloc);
+
     res.status(200).json({
-    message: 'Data received successfully on the server!',
-    dataYouSent: receivedData
-  
+        message: 'Data received successfully on the server!',
+        dataYouSent: currentusrloc
+    });
 });
+
+app.listen(port, () => {
+    console.log(`Server started listening on: ${port}`);
 });
-app.listen(port, ()=> {
-    
-    console.log(`Server started listening on :${port}`);
-})
+
